b6d455394af8bb09f87af4087d27ebe57e3ed176
KAZUM1 <51043463+KAZUM1@users.noreply.github.com>
2025-02-16 11:59:10 +0300

    Update apps.py
    подключил signals для автоматического создания оценки после бронирования

--- queue_system/queue_app/apps.py
+++ queue_system/queue_app/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class QueueAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'queue_app'
+
+class QueueAppConfig(AppConfig):
+    def ready(self):
+        import queue_app.signals
